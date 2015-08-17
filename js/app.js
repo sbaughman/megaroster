8cc542ba9ffc8d58b846fefb11afeacfe9fdcf31
Seth Baughman <sbaughman1@me.com>
2015-08-17 16:16:55 -0400

    Add student names to the array and HTML list

---
+++ js/app.js
@@ -0,0 +1,6 @@
+requirejs([
+  "https://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.4/jquery.js",
+  "https://cdnjs.cloudflare.com/ajax/libs/jqueryui/1.11.4/jquery-ui.js",
+  "https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.5/js/bootstrap.js",
+  "js/megaroster.js"
+]);
--- js/megaroster.js
+++ js/megaroster.js
@@ -1,7 +1,24 @@
 var Megaroster = function() {
+
   this.init = function() {
-    alert('Whaaaat?');
+    // set self equal to 'this' so that it can be used in the next function
+    var self = this;
+    this.students = [];
+
+    $('#new_student_form').on('submit', function(ev) {
+      ev.preventDefault();
+      // gets value from student_name input field
+      var student_name = $(this.student_name).val();
+      // pushes value from form into students array
+      self.students.push(student_name);
+      console.log(self.students);
+      // add the student to a new list item in the <ol>
+      $('#students').append('<li class="list-group-item">' + student_name + '</li>');
+      this.student_name.focus();
+      this.student_name.select();
+    });
   };
+
 };
 
 var roster = new Megaroster();
