1c28effc74fa6c91df8924b56cc48b2a28fd51ff
Seth Baughman <sbaughman1@me.com>
2015-08-17 17:45:53 -0400

    Save to and load from localStorage.

--- js/megaroster.js
+++ js/megaroster.js
@@ -1,25 +1,52 @@
 var Megaroster = function() {
+  var self = this;
+
+  this.save = function() {
+    try {
+      return (localStorage.students = JSON.stringify(self.students));
+    }
+    catch(err) {
+      return false;
+    }
+  };
+
+  this.load = function() {
+    try {
+      self.students = JSON.parse(localStorage.students);
+      $.each(self.students, function(index, student_name) {
+        self.appendToList(student_name);
+      });
+    }
+    catch(err) {
+      return false;
+    }
+  };
+
+  this.appendToList = function(student_name) {
+    // append an <li> with student name to <ol>
+    $('#students').append('<li class="list-group-item">' + student_name + '</li>');
+  };
+
+  this.addStudent = function(student_name) {
+    // pushes value from form into students array
+    self.students.push(student_name);
+    self.appendToList(student_name);
+    self.save();
+  };
 
   this.init = function() {
-    // set self equal to 'this' so that it can be used in the next function
-    var self = this;
-    this.students = [];
+    self.students = [];
+    self.load();
+
 
     $('#new_student_form').on('submit', function(ev) {
       ev.preventDefault();
       // gets value from student_name input field
       var student_name = $(this.student_name).val().trim();
-      // if text in entry form, pushes value from form into students array
-      if (student_name) {
-        self.students.push(student_name);
-        // add the student to a new list item in the <ol>
-        $('#students').append('<li class="list-group-item">' + student_name + '</li>');
-        // clear entry field and focus
-        $(this.student_name).val('').focus();
-      }
-      else {
-        alert('Enter a valid name');
-      }
+
+      self.addStudent(student_name);
+      // clear entry field and focus
+      $(this.student_name).val('').focus();
     });
   };
 
