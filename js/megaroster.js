40457736a9624ae085cac1cf3c0a1d7a34534f3e
Seth Baughman <sbaughman1@me.com>
2015-08-17 16:53:29 -0400

    Clear and focus the name field after adding student.

--- js/megaroster.js
+++ js/megaroster.js
@@ -8,14 +8,18 @@ var Megaroster = function() {
     $('#new_student_form').on('submit', function(ev) {
       ev.preventDefault();
       // gets value from student_name input field
-      var student_name = $(this.student_name).val();
-      // pushes value from form into students array
-      self.students.push(student_name);
-      console.log(self.students);
-      // add the student to a new list item in the <ol>
-      $('#students').append('<li class="list-group-item">' + student_name + '</li>');
-      this.student_name.focus();
-      this.student_name.select();
+      var student_name = $(this.student_name).val().trim();
+      // if text in entry form, pushes value from form into students array
+      if (student_name) {
+        self.students.push(student_name);
+        // add the student to a new list item in the <ol>
+        $('#students').append('<li class="list-group-item">' + student_name + '</li>');
+        // clear entry field and focus
+        $(this.student_name).val('').focus();
+      }
+      else {
+        alert('Enter a valid name');
+      }
     });
   };
 
