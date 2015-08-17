e0067733117be1a3bb51fb7c73cbf8bd515019ab
Seth Baughman <sbaughman1@me.com>
2015-08-17 11:53:17 -0400

    Use a constructor pattern.

--- js/megaroster.js
+++ js/megaroster.js
@@ -1,12 +1,8 @@
-var Megatask = {
-  author: "Seth",
-  newStudentForm: $('#new_student_form'),
-  submitHandler: function() {
-    alert('Whaaaaat?!');
-  },
-  start: function() {
-    this.newStudentForm.submit(this.submitHandler);
-  }
+var Megaroster = function() {
+  this.init = function() {
+    alert('Whaaaat?');
+  };
 };
 
-Megatask.start();
+var roster = new Megaroster();
+roster.init();
